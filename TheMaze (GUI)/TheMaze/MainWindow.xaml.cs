db355cdb50c54438580885f67528ce28b86e9331
pofka94 <45260470+pofka94@users.noreply.github.com>
2020-04-21 23:24:52 +0100

    Add files via upload

--- TheMaze (GUI)/TheMaze/MainWindow.xaml.cs
+++ TheMaze (GUI)/TheMaze/MainWindow.xaml.cs
@@ -26,68 +26,56 @@ namespace TheMaze
             Rooms.Walking();    //Used to set all the values for the program to use
             ColouringRooms();   //Colours the block that you start on
         }
-
         public void ColouringRooms()
         {
             var colour = Brushes.LightGray; //Used for quick colour changing
             int yAxis = Rooms.yAxis;        //Used to define yAxis from Rooms class
             int xAxis = Rooms.xAxis;        //Used to define xAxis from Rooms class
-
             //All of this is used to check on what block is the player standing on and will repaint it to a certain colour
             if (yAxis == 1 && xAxis == 1) _1_1.Background = colour;
             if (yAxis == 1 && xAxis == 2) _1_2.Background = colour;
             if (yAxis == 1 && xAxis == 3) _1_3.Background = colour;
             if (yAxis == 1 && xAxis == 4) _1_4.Background = colour;
             if (yAxis == 1 && xAxis == 5) _1_5.Background = colour;
-                                                                           
             if (yAxis == 2 && xAxis == 1) _2_1.Background = colour;
             if (yAxis == 2 && xAxis == 2) _2_2.Background = colour;
             if (yAxis == 2 && xAxis == 3) _2_3.Background = colour;
             if (yAxis == 2 && xAxis == 4) _2_4.Background = colour;
             if (yAxis == 2 && xAxis == 5) _2_5.Background = colour;
-                                                                        
             if (yAxis == 3 && xAxis == 1) _3_1.Background = colour;
             if (yAxis == 3 && xAxis == 2) _3_2.Background = colour;
             if (yAxis == 3 && xAxis == 3) _3_3.Background = colour;
             if (yAxis == 3 && xAxis == 4) _3_4.Background = colour;
             if (yAxis == 3 && xAxis == 5) _3_5.Background = colour;
-                                                                             
             if (yAxis == 4 && xAxis == 1) _4_1.Background = colour;
             if (yAxis == 4 && xAxis == 2) _4_2.Background = colour;
             if (yAxis == 4 && xAxis == 3) _4_3.Background = colour;
             if (yAxis == 4 && xAxis == 4) _4_4.Background = colour;
             if (yAxis == 4 && xAxis == 5) _4_5.Background = colour;
-                                                                             
             if (yAxis == 5 && xAxis == 1) _5_1.Background = colour;
             if (yAxis == 5 && xAxis == 2) _5_2.Background = colour;
             if (yAxis == 5 && xAxis == 3) _5_3.Background = colour;
             if (yAxis == 5 && xAxis == 4) _5_4.Background = colour;
             if (yAxis == 5 && xAxis == 5) _5_5.Background = colour;
         }   //Repaints blocks that have been discovered/been in
-
         public void ButtonUp_Click(object sender, RoutedEventArgs e)
         {
             //Most of this code is repeated to other buttons but with different booleans and such
             Rooms.canUp = true; //Sets a boolean canUp to true in the Room class to see if the player can move up
             Rooms.Walking();    //Does all of the walking
             ColouringRooms();   //Does all of the repainting on the UI
-
             //Gets random text for an impossible move (trying to move to a wall) It will be sent to the black textbox
             if (Rooms.cantMove == true) TextBlock.Text = Rooms.WallText(rng.Next(0, 2));
             else TextBlock.Text = "";
-
             Rooms.cantMove = true;  //Resets the boolean
         }       //Does things when the Up button is clicked
-
         private void ButtonDown_Click(object sender, RoutedEventArgs e)
         {
             Rooms.canDown = true;
             Rooms.Walking();
             ColouringRooms();
-
             if (Rooms.cantMove == true) TextBlock.Text = Rooms.WallText(rng.Next(0, 2));
             else TextBlock.Text = "";
-
             Rooms.cantMove = true;
             if (Rooms.Win)
             {
@@ -95,16 +83,13 @@ namespace TheMaze
                 System.Windows.Application.Current.Shutdown();
             }
         }    //Does things when the Down button is clicked
-
         private void ButtonRight_Click(object sender, RoutedEventArgs e)
         {
             Rooms.canRight = true;
             Rooms.Walking();
             ColouringRooms();
-
             if (Rooms.cantMove == true) TextBlock.Text = Rooms.WallText(rng.Next(0, 2));
             else TextBlock.Text = "";
-
             Rooms.cantMove = true;
             if (Rooms.Win)
             {
@@ -112,19 +97,15 @@ namespace TheMaze
                 System.Windows.Application.Current.Shutdown();
             }
         }   //Does things when the Right button is clicked
-
         private void ButtonLeft_Click(object sender, RoutedEventArgs e)
         {
             Rooms.canLeft = true;
             Rooms.Walking();
             ColouringRooms();
-
             if (Rooms.cantMove == true) TextBlock.Text = Rooms.WallText(rng.Next(0, 2));
             else TextBlock.Text = "";
-
             Rooms.cantMove = true;
         }    //Does things when the Left button is clicked
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show(
--- TheMaze (GUI)/TheMaze/Rooms.cs
+++ TheMaze (GUI)/TheMaze/Rooms.cs
@@ -3,36 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace TheMaze
 {
     class Rooms
     {
         public static Random rng = new Random();
-
         public static int person = 5;       //Players coordination number
-
         public static int rows = 7;     //total number of rows in the grid
         public static int collumns = 7; //total number of collumns in the grid
-
         public static int[,] grid = new int[rows, collumns];    //creates an empty grid
-
         public static bool canUp = false;
         public static bool canDown = false;
         public static bool canLeft = false;
         public static bool canRight = false;
         public static bool Win = false;
-
         public static bool cantMove = false;
-
         public static int yAxis = 1;    //for WPF to recognize where the player's yAxis is
         public static int xAxis = 1;    //for WPF to recognize where the player's xAxis is
-
         public static void GRooms()     //creates a custom/semi-random maze maze grid
         {
             int none = 0;
             int exit = 4;
-
             grid[0, 0] = none;  grid[0, 1] = none;              grid[0, 2] = none;              grid[0, 3] = none;              grid[0, 4] = none;              grid[0, 5] = none;              grid[0, 6] = none;
             grid[1, 0] = none;  grid[1, 1] = 1;                 grid[1, 2] = 1;                 grid[1, 3] = rng.Next(0, 2);    grid[1, 4] = rng.Next(0, 2);    grid[1, 5] = rng.Next(0, 2);    grid[1, 6] = none;
             grid[2, 0] = none;  grid[2, 1] = rng.Next(0, 2);    grid[2, 2] = 1;                 grid[2, 3] = 1;                 grid[2, 4] = 1;                 grid[2, 5] = 1;                 grid[2, 6] = none;
@@ -41,26 +32,21 @@ namespace TheMaze
             grid[5, 0] = none;  grid[5, 1] = rng.Next(0, 2);    grid[5, 2] = rng.Next(0, 2);    grid[5, 3] = rng.Next(0, 2);    grid[5, 4] = rng.Next(0, 2);    grid[5, 5] = exit;              grid[5, 6] = none;
             grid[6, 0] = none;  grid[6, 1] = none;              grid[6, 2] = none;              grid[6, 3] = none;              grid[6, 4] = none;              grid[6, 5] = none;              grid[6, 6] = none;
         }
-
         public static void Walking()    //Allows the player to move
         {
             int pastRoomNumber = 0;
-
             bool loopDone = false;
-
             for (int y = yAxis; !loopDone && !Win;)          //creates a loop for the y axis
-            {
+            { 
                 for (int x = xAxis; !loopDone && !Win;)      //creates a loop for the x axis
                 {
                     pastRoomNumber = grid[y, x];
                     if (grid[y, x] == grid[5, 5]) Win = true;
-
                     string move = null;
                     if (canUp) move = "u";      //Gets the boolean from the button and uses it on switch
                     if (canDown) move = "d";
                     if (canLeft) move = "l";
                     if (canRight) move = "r";
-
                     switch (move)
                     {
                         case "r":
@@ -77,7 +63,6 @@ namespace TheMaze
                                 cantMove = true;
                             }
                             break;
-
                         case "l":
                             if (grid[y, x - 1] != 0)
                             {
@@ -91,7 +76,6 @@ namespace TheMaze
                                 cantMove = true;
                             }
                             break;
-
                         case "u":
                             if (grid[y - 1, x] != 0)
                             {
@@ -105,7 +89,6 @@ namespace TheMaze
                                 cantMove = true;
                             }
                             break;
-
                         case "d":
                             if (grid[y + 1, x] != 0) 
                             {
@@ -120,46 +103,36 @@ namespace TheMaze
                             }
                             break;
                     }       //Checks which boolean is true and executes the relevant code
-
                     yAxis = y;  //Sets the yAxis as the current possition
                     xAxis = x;  //Same here for the xAxis
-
                     loopDone = true;    //It sets it to true so the loop could finish
                 }
             }   //These 2 for loops are used to move and know where the player is standing
         }
-
         public static string WallText(int pickText)
         {
             string aWall = null;
-
             switch (pickText)
             {
                 case 0: aWall = "You see a wall in front of you.";
                     break;
-
                 case 1: aWall = "There is a Wall... You can't pass through it.";
                     break;
             }
-
             return aWall;
         }   //Get's a random text if you were to try to move into a wall
-
         public static string winningMessage(int pickWinText)
         {
             string win = null;
-
             switch (pickWinText)
             {
                 case 0:
                     win = "You won!";
                     break;
-
                 case 1:
                     win = "Pff, I guess you won this time!";
                     break;
             }
-
             return win;
         }   //Get's a random winning message when you reach the finishing line
     }
